0ca32a66941dda29bf5e2a72cfe03361ca707477
Taiming Liu <liutaiming3@gmail.com>
2023-04-27 23:39:07 -0700

    Initial Commit

---
+++ iOS_messenger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS_messenger
+//
+//  Created by Taiming Liu on 4/27/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
